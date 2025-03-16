8993af66030f849eb82c9f6ea246567c56dc4bb7
Gilberto Massulo Neto <gilbertomassulo@gmail.com>
2025-03-16 17:41:57 -0300

    Changed to EXPO

--- aula05/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parserOptions: {
-    // Desabilita a exigência de um arquivo de configuração Babel para cada arquivo analisado
-    requireConfigFile: false,
-  },
-  // Outras configurações do ESLint...
-};
