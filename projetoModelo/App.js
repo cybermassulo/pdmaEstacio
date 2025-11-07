82ff56663b83086b46bd003243166296c39bb706
Gilberto Massulo Neto <gilbertomassulo@gmail.com>
2025-11-07 10:16:54 -0300

    upadates nos projetos da aula

--- projetoModelo/App.js
+++ projetoModelo/App.js
@@ -1,15 +1,21 @@
+import 'react-native-reanimated';      
+import 'react-native-gesture-handler';
+
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import AppDrawer from './src/components/navigation/AppDrawer';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
     <>
       <StatusBar hidden />
-      <NavigationContainer>
-        <AppDrawer />
-      </NavigationContainer>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <NavigationContainer>
+            <AppDrawer />
+          </NavigationContainer>
+        </GestureHandlerRootView>
     </>
   );
 }
